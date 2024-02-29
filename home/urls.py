5ecbba6f9ccbc8a3733cb31aa99a1a4b847aefcb
KarmaSherpaa <np03cs4a210053@heraldcollege.edu.np>
2024-02-29 20:21:03 +0545

    required files

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("destination/", views.destination_detail, name="destination_detials"),  # Add this line
+    path('destination/', views.destination_detail, name='destination_details'),  # Remove the trailing slash
     path('destination/<int:destination_id>/', views.destination_detail, name='destination_detail'),
-]   
+]
